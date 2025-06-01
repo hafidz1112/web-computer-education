a9c5a9a9ba4defe14386e64000a385a763b62dc7
hafidz1112 <153370411+hafidz1112@users.noreply.github.com>
2025-06-01 07:00:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
-    const navbar = document.querySelector(".navbar");
 
-    menuIcon.addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    });
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
 });
