52729192c51580c7987538069d6b511be5d71235
hafidz1112 <153370411+hafidz1112@users.noreply.github.com>
2025-06-03 13:15:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,37 @@ const menuIcon = document.getElementById("menu-icon");
 const menuList = document.getElementById("menu-list");
 
 menuIcon.addEventListener("click", () => {
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 2, // default di desktop
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1 // untuk layar kecil, misalnya HP
+            },
+            768: {
+                slidesPerView: 3 // untuk layar tablet ke atas
+            }
+        }
+    });
+});
+
+
     menuList.classList.toggle("hidden");
 });
